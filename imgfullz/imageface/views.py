8068235665253bc4cea7668ad865596a3ba3133c
Tuan Tran <tuantt13@wru.vn>
2019-02-13 15:55:01 +0700

    Created url routing, base template

--- imgfullz/imageface/views.py
+++ imgfullz/imageface/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
 
-# Create your views here.
+from .models import Post
+
+
+def imageface_index(request):
+    posts = Post.objects.filter(
+        published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'imageface_index.html')
